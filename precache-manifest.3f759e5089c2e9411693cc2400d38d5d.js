334bb313b5ac62d4af27e813fac6b5321ebce01b
Javier Infante <jabi@irontec.com>
2018-10-29 01:23:05 +0100

    Updates

---
+++ precache-manifest.3f759e5089c2e9411693cc2400d38d5d.js
@@ -0,0 +1,325 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c75ba648fe27b9fe6a028d50593272f",
+    "url": "media/beacons.jpg"
+  },
+  {
+    "revision": "27ddf47c7a7de1b45858d6d59a4017f5",
+    "url": "notes.html"
+  },
+  {
+    "revision": "5af28023f28d936a66d54ff1bf1e3d67",
+    "url": "media/bmf.png"
+  },
+  {
+    "revision": "54df1751e387c52b4711",
+    "url": "hljs-langs/bash.js"
+  },
+  {
+    "revision": "d5c1a492e837701e80c3",
+    "url": "hljs-langs/javascript.js"
+  },
+  {
+    "revision": "fe22cb1001b91f57a6d49106c94e06e0",
+    "url": "index.html"
+  },
+  {
+    "revision": "ccb01974b04d60fa6e7008ad33f028c0",
+    "url": "media/euskaltweetron.mp4"
+  },
+  {
+    "revision": "6b31d6b8a924eabd1abb3c4e039c1d82",
+    "url": "media/foa2014.mp4"
+  },
+  {
+    "revision": "0dd9b3f9e7ccffcb319daec205890177",
+    "url": "media/zorionak.mp4"
+  },
+  {
+    "revision": "a45bf3dd9e3b438a3b4a31ebf7c99061",
+    "url": "media/Irontec_tu villancico mecanizado.mp4"
+  },
+  {
+    "revision": "f2eda15a4e0151ad4f6ed47f86b05e5e",
+    "url": "media/homer.gif"
+  },
+  {
+    "revision": "583ef660a69d5110e17db2cdfbc3aba8",
+    "url": "media/megaselfie.mp4"
+  },
+  {
+    "revision": "b02596aee4d943c3be6b324d8c4313c8",
+    "url": "media/IMG_9695.JPG"
+  },
+  {
+    "revision": "bfde5187774a71b5125f8e316b6bfa8b",
+    "url": "media/equipoa.gif"
+  },
+  {
+    "revision": "e519e35c3f4bc22eba937ffca5f9b85c",
+    "url": "media/minicong_final.jpg"
+  },
+  {
+    "revision": "f1fa69a5a1b077882cc8bd36ef5a57ab",
+    "url": "media/llamcus.jpg"
+  },
+  {
+    "revision": "4e3f813557645557caebee31300cbc3e",
+    "url": "media/minicong_explained.png"
+  },
+  {
+    "revision": "3202a5878a6420bdfd0fc02a0f3080ec",
+    "url": "media/SioFus_montaje.jpg"
+  },
+  {
+    "revision": "d82d6053262ae64061de26e0befaabbc",
+    "url": "media/bl2.jpg"
+  },
+  {
+    "revision": "0074e2ce260416fe920e00690c63a2a8",
+    "url": "media/bricomania.jpg"
+  },
+  {
+    "revision": "0b334ad811eb9134ee4e437acc18c71f",
+    "url": "media/bl3.jpg"
+  },
+  {
+    "revision": "f710cc0b356f47fe2f2cac340a5542c4",
+    "url": "media/mercedes.jpg"
+  },
+  {
+    "revision": "9209746cc3f056d932f6103f5e2d036a",
+    "url": "media/minicong_twitter.png"
+  },
+  {
+    "revision": "e9a4f9a8e370693e658ea0f7c57c1064",
+    "url": "media/ucs.jpg"
+  },
+  {
+    "revision": "cee6d6a62360e1f545a2bd309686edc4",
+    "url": "media/brokentooth.jpg"
+  },
+  {
+    "revision": "2a3c2c69496c8ee4133e4c135b95a3f6",
+    "url": "media/bilbao.jpg"
+  },
+  {
+    "revision": "bf1f2afe8b9c786eac5ccd2299a2d431",
+    "url": "media/zorionak2.jpg"
+  },
+  {
+    "revision": "56f3bd312d6ba02ba8f83ce1e8c13e66",
+    "url": "media/pack.jpg"
+  },
+  {
+    "revision": "34249157beddd2f405a12d6e953116cf",
+    "url": "media/reles.jpg"
+  },
+  {
+    "revision": "16a184c86a1aceaffe01b75d84bb36ac",
+    "url": "media/pedal2.jpg"
+  },
+  {
+    "revision": "c5e74152a707a5ba2ef642c9aca2b904",
+    "url": "media/logo_irontec.png"
+  },
+  {
+    "revision": "8a20fbe7e86ef2b703c241825500650d",
+    "url": "media/keyboard.jpg"
+  },
+  {
+    "revision": "10ca985aa01fd92c237b99e7198182e9",
+    "url": "media/bandenlehia.jpg"
+  },
+  {
+    "revision": "33ade5b6817f0a0e7dd292db526c234a",
+    "url": "media/euskaltweetron.jpg"
+  },
+  {
+    "revision": "1d4f972ee08744d2c435c4a8771802d6",
+    "url": "media/everyday.jpg"
+  },
+  {
+    "revision": "ddd534051bd79bd334d8fafc24776b74",
+    "url": "media/fondo_presentacion.jpg"
+  },
+  {
+    "revision": "c319a7481900f9a3bd570db2610ae693",
+    "url": "media/how.jpg"
+  },
+  {
+    "revision": "9978985c163d50648d7a7484b5d6816c",
+    "url": "media/minicong.jpg"
+  },
+  {
+    "revision": "9fd777ceba4572b291b51b1536096a90",
+    "url": "media/photocall1.jpg"
+  },
+  {
+    "revision": "bb890cdbfc295fcfd20423ca8c1aaa68",
+    "url": "media/photocall2.jpg"
+  },
+  {
+    "revision": "9b78f90247cd5fd9d7847462fcd8e5e8",
+    "url": "media/scatron.jpg"
+  },
+  {
+    "revision": "234d7c6f645260e6ec8905965beac4e0",
+    "url": "media/zorionak.jpg"
+  },
+  {
+    "revision": "fd34d0e97079e19045756f5420381df8",
+    "url": "media/abrebotellas.jpg"
+  },
+  {
+    "revision": "ec750ed4f008bbcf9c79",
+    "url": "hljs-langs/cpp.js"
+  },
+  {
+    "revision": "738ebcb538f65d56d431756b2ca80d7c",
+    "url": "media/email.png"
+  },
+  {
+    "revision": "41a838a4cacd41ebc90ad399f9645063",
+    "url": "media/github.png"
+  },
+  {
+    "revision": "ffc36c72ad0dd4e6287f64a51c1c29ba",
+    "url": "media/goonies.jpg"
+  },
+  {
+    "revision": "19f00351f0e52d1459584463a13ebcaa",
+    "url": "media/macgyver.jpg"
+  },
+  {
+    "revision": "cec36bb301770049aa3d0f2999207368",
+    "url": "media/rele.jpg"
+  },
+  {
+    "revision": "e9d3905ff5416275fcb51825d05bea98",
+    "url": "media/usb2serial.jpg"
+  },
+  {
+    "revision": "30f4f12773dda5fbce1416e4f7760a85",
+    "url": "media/diente.jpg"
+  },
+  {
+    "revision": "777feaf81832ef256fff7c49ed9486e5",
+    "url": "media/equipoa.jpg"
+  },
+  {
+    "revision": "728c3add567a13bcf6cc394fe31c90d3",
+    "url": "media/raton1.jpg"
+  },
+  {
+    "revision": "a7783d33c01e2cc6663abc67f29502fe",
+    "url": "media/twitter.png"
+  },
+  {
+    "revision": "850913ce8661f8a376dfb8a023fe202c",
+    "url": "media/pedal.jpg"
+  },
+  {
+    "url": "ee7b96fa85d8fdb8c126409326ac2d2b.ttf"
+  },
+  {
+    "revision": "e1c2a13c94fddc30a771",
+    "url": "app.js"
+  },
+  {
+    "url": "ddd534051bd79bd334d8fafc24776b74.jpg"
+  },
+  {
+    "revision": "9b78f90247cd5fd9d7847462fcd8e5e8",
+    "url": "d96cd9620fde69f7bda4164520bd9390.jpg"
+  },
+  {
+    "revision": "6e29ff6d4e03bd5ba8b3d5556174789e",
+    "url": "b97c530867a61ea60506b10a416762c4.svg"
+  },
+  {
+    "url": "fc84e998bc29b297ea20321e4c90b6ed.ttf"
+  },
+  {
+    "url": "ec4c9962ba54eb91787aa93d361c10a8.ttf"
+  },
+  {
+    "url": "d1efcd4d126837fe0dcf9b6cf3a00d64.ttf"
+  },
+  {
+    "revision": "9978985c163d50648d7a7484b5d6816c",
+    "url": "f5b33c0aec418a54645527381328f3ec.jpg"
+  },
+  {
+    "revision": "c319a7481900f9a3bd570db2610ae693",
+    "url": "0f1976dbe2f68ed3c58ff62006e315b6.jpg"
+  },
+  {
+    "revision": "e1c2a13c94fddc30a771",
+    "url": "app.css"
+  },
+  {
+    "revision": "e688707ed86fca73e90fd1e2d3dc4645",
+    "url": "a0296bb86117cab30ef27fd515a87f79.svg"
+  },
+  {
+    "url": "1eb7a893589ddce89d81cdb22a356660.ttf"
+  },
+  {
+    "revision": "1d4f972ee08744d2c435c4a8771802d6",
+    "url": "240366a598ea3bbe7c0cce5a814360fb.jpg"
+  },
+  {
+    "revision": "8ec6268e47b6b2ca18a0b054471be6b4",
+    "url": "3rd.party.LICENSES"
+  },
+  {
+    "revision": "bb890cdbfc295fcfd20423ca8c1aaa68",
+    "url": "47e1667b45fc2bc52147442d6e0451ad.jpg"
+  },
+  {
+    "url": "9fd777ceba4572b291b51b1536096a90.jpg"
+  },
+  {
+    "url": "89e2666c24d37055bcb60e9d2d9f7e35.ttf"
+  },
+  {
+    "url": "3e1af3ef546b9e6ecef9f3ba197bf7d2.ttf"
+  },
+  {
+    "url": "50705c5ed1205b63efdbfee941a6b655.ttf"
+  },
+  {
+    "url": "42bbe4eefcde1297b11dc4b6491e9746.ttf"
+  },
+  {
+    "revision": "33ade5b6817f0a0e7dd292db526c234a",
+    "url": "4becceab56f6a7cdbac3b3ff36f59f31.jpg"
+  },
+  {
+    "revision": "234d7c6f645260e6ec8905965beac4e0",
+    "url": "82a317827f1dc2f2ba29c9c889404764.jpg"
+  },
+  {
+    "revision": "8a63aaf795c65b9a001dfc6bf7387293",
+    "url": "66cec3167484d42c1ccc513c6259a4f8.svg"
+  },
+  {
+    "revision": "9d9be852aec6202f99a33cf9cc292d9c",
+    "url": "95c7b5a26ab51d96ee39f36c66005d67.svg"
+  },
+  {
+    "revision": "5d039d9f3408836ef24841905f04b69b",
+    "url": "1ba934df20c60bfbda92ae00e4ac1b37.svg"
+  },
+  {
+    "revision": "43b843880f019c3bdf2a047b201961a7",
+    "url": "187a99d3fe4e62c75737c5f4a93e00a6.svg"
+  },
+  {
+    "url": "10ca985aa01fd92c237b99e7198182e9.jpg"
+  },
+  {
+    "url": "0fc25386220a58203994ce45fb4ae570.ttf"
+  }
+];
